cde201d45ed678a10674a2cc524d0c75fab489dc
doguto <doguto110@gmail.com>
2025-05-27 16:39:08 +0900

    CurrentUserの処理をCurrentAttributeを使うように変更

--- app/domains/page/auth_page/authenticate_user_domain.rb
+++ app/domains/page/auth_page/authenticate_user_domain.rb
@@ -1,14 +1,11 @@
 module Page::AuthPage
   class AuthenticateUserDomain < ApplicationDomain
-    def initialize(token)
-      super()
-      @token = token
-    end
-
-    def execute
-      decoded_token = JsonWebToken.decode(@token)
+    def execute(token)
+      decoded_token = JsonWebToken.decode(token)
       user = User.find_by(id: decoded_token[:user_id])
       raise "Unauthorized" if user.nil?
+
+      Current.current_user = user unless user.nil?
 
       user
     end
