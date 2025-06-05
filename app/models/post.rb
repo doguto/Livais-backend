ea6280b42d9f68f8960222baf8e3e3d77059fced
tsukuneA1 <tkschr1125@gmail.com>
2025-06-06 00:18:57 +0900

    fix:fixed format and relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,11 @@ class Post < ApplicationRecord
 
   has_many :likes, inverse_of: :post, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
-  has_many :current_user_likes, -> { where(user: Current.current_user) }, class_name: "Like", inverse_of: :post
+  has_many :current_user_likes, -> { where(user: Current.current_user) }, class_name: "Like", inverse_of: :post, dependent: :destroy
 
   has_many :reposts, inverse_of: :post, dependent: :destroy
   has_many :reposting_user, through: :reposts, source: :user
-  has_many :current_user_reposts, -> { where(user: Current.current_user) }, class_name: "Repost", inverse_of: :post
+  has_many :current_user_reposts, -> { where(user: Current.current_user) }, class_name: "Repost", inverse_of: :post, dependent: :destroy
 
   belongs_to :parent_post, class_name: "Post", foreign_key: "reply_to_id", optional: true, inverse_of: :replies, counter_cache: :replies_count
   has_many :replies, class_name: "Post", foreign_key: "reply_to_id", dependent: :destroy, inverse_of: :parent_post
