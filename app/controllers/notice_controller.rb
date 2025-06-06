76563d4794eda6fc73906140022370e70b32f9cb
doguto <doguto110@gmail.com>
2025-06-06 16:10:43 +0900

    Feat: Noticeの導線作成

--- app/controllers/notice_controller.rb
+++ app/controllers/notice_controller.rb
@@ -1,6 +1,6 @@
 class NoticeController < ApplicationController
   def show
-    dtos = ShowNoticeDomain.new.execute
+    dtos = Page::NoticePage::ShowNoticeDomain.new.execute
     render json: dtos.map(&:get), status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "notice/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -30,5 +29,6 @@ Rails.application.routes.draw do
     post "auth/google", to: "auth#google"
 
     get "auth/me", to: "auth#me"
+    get "notice/show"
   end
 end
