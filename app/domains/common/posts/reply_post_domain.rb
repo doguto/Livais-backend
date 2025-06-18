3b665fa883f5dbc224240c63b11db88356ce843b
doguto <doguto110@gmail.com>
2025-06-18 16:51:59 +0900

    Fix: 無効なPostだった際にErrorを発火するように修正

--- app/domains/common/posts/reply_post_domain.rb
+++ app/domains/common/posts/reply_post_domain.rb
@@ -2,7 +2,7 @@ module Common::Posts
   class ReplyPostDomain < ApplicationDomain
     def execute(content:, reply_to_id:)
       user = Current.current_user
-      post = Post.create(user: user, content: content)
+      post = Post.create!(user: user, content: content)
       replied_post = Post.find(reply_to_id)
 
       is_following_user = user.following.exists?(id: replied_post.user_id)
