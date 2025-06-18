64fa6a743158aa26a21af1af2f4e6946ffb6b682
doguto <doguto110@gmail.com>
2025-06-18 16:43:03 +0900

    Fix: Replyテーブル込みで返信を可能に

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,9 +11,9 @@ class Post < ApplicationRecord
   has_many :current_user_reposts, -> { where(user: Current.current_user) }, class_name: "Repost", inverse_of: :post, dependent: :destroy
 
   has_one :parent_reply, class_name: "Reply", inverse_of: :child_post, dependent: :destroy
-  has_one :parent_post, class_name: "Post", through: :parent_reply, inverse_of: :child_posts, dependent: :destroy
+  has_one :parent_post, class_name: "Post", through: :parent_reply
   has_many :child_replies, class_name: "Reply", inverse_of: :parent_post, dependent: :destroy, counter_cache: :replies_count
-  has_many :child_posts, class_name: "Post", through: :child_replies, inverse_of: :parent_post, dependent: :destroy
+  has_many :child_posts, class_name: "Post", through: :child_replies, source: :child_post
 
   belongs_to :quoted_post, class_name: "Post", optional: true, counter_cache: :quotes_count
 
