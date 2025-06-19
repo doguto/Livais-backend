36f248efff6f568ec640ea83814d98b7027a0fe0
doguto <doguto110@gmail.com>
2025-06-19 21:38:08 +0900

    Test: 通知リスト取得のテストを修正

--- test/controllers/notice_controller_test.rb
+++ test/controllers/notice_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class NoticeControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get notice_show_url
+  test "should get index" do
+    get notice_index_url
     assert_response :success
   end
 end
