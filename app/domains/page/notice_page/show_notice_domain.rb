d856b2438bfa9affa1745a99b15ea59150d24fe5
doguto <doguto110@gmail.com>
2025-06-19 21:07:45 +0900

    Feat: 通知の型の種類が増えたのでDomainを更新

--- app/domains/page/notice_page/show_notice_domain.rb
+++ app/domains/page/notice_page/show_notice_domain.rb
@@ -1,7 +1,9 @@
 module Page::NoticePage
   class ShowNoticeDomain < ApplicationDomain
-    POST_NOTICE_TYPE = ["Repost", "Like", "Post"].freeze
+    POST_NOTICE_TYPE = ["Repost", "Like"].freeze
     FOLLOW_NOTICE_TYPE = ["Follow"].freeze
+    REPLY_TYPE = ["Reply"].freeze
+    QUOTE_TYPE = ["Quote"].freeze
 
     def execute
       notices = Notice.where(user_id: Current.current_user&.id, is_hide: false).includes(:notifiable).order(created_at: :desc)
@@ -14,6 +16,16 @@ module Page::NoticePage
           from_user = User.find_by(id: notice.notifiable.user_id)
           post = Post.find_by(id: notice.notifiable.post_id)
           dtos << NoticeDto.new(notice, user: from_user, post: post)
+        elsif REPLY_TYPE.include?(notice.notifiable_type)
+          from_user = User.find_by(id: notice.notifiable.child_post.user_id)
+          post = Post.find_by(id: notice.notifiable.child_post_id)
+          dtos << NoticeDto.new(notice, user: from_user, post: post)
+        elsif QUOTE_TYPE.include?(notice.notifiable_type)
+          from_user = User.find_by(id: notice.notifiable.quoting_post.user_id)
+          post = Post.find_by(id: notice.notifiable.quoting_post_id)
+          dtos << NoticeDto.new(notice, user: from_user, post: post)
+        else
+          dtos << NoticeDto.new(notice)
         end
       end
       dtos
