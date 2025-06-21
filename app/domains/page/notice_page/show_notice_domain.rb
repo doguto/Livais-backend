79561b6b40948917fefdf7c363cd612cb65bee18
doguto <doguto110@gmail.com>
2025-06-21 15:39:12 +0900

    Fix: dtoに渡す引数が違ったので修正

--- app/domains/page/notice_page/show_notice_domain.rb
+++ app/domains/page/notice_page/show_notice_domain.rb
@@ -9,13 +9,13 @@ module Page::NoticePage
       notices = Notice.where(user_id: Current.current_user&.id, is_hide: false).includes(:notifiable).order(created_at: :desc)
       notices.map do |notice|
         if FOLLOW_NOTICE_TYPE.include?(notice.notifiable_type)
-          NoticeDto.new(notice, user_id: notice.notifiable.follower_id)
+          NoticeDto.new(notice, user: notice.notifiable.follower)
         elsif POST_NOTICE_TYPE.include?(notice.notifiable_type)
-          NoticeDto.new(notice, user_id: notice.notifiable.user_id, post_id: notice.notifiable.post_id)
+          NoticeDto.new(notice, user: notice.notifiable.user, post: notice.notifiable.post)
         elsif REPLY_TYPE.include?(notice.notifiable_type)
-          NoticeDto.new(notice, user_id: notice.notifiable.child_post.user_id, post_id: notice.notifiable.child_post_id)
+          NoticeDto.new(notice, user: notice.notifiable.child_post.user, post: notice.notifiable.child_post)
         elsif QUOTE_TYPE.include?(notice.notifiable_type)
-          NoticeDto.new(notice, user_id: notice.notifiable.quoting_post.user_id, post_id: notice.notifiable.quoting_post_id)
+          NoticeDto.new(notice, user: notice.notifiable.quoting_post.user, post: notice.notifiable.quoting_post)
         else
           NoticeDto.new(notice)
         end
