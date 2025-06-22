077ed3b772e80c7fc845c7b4560953375dc32196
doguto <doguto110@gmail.com>
2025-06-22 22:20:49 +0900

    Fix: NoticeSettingのUserをForeignKeyに

--- db/migrate/20250622041650_create_notice_settings.rb
+++ db/migrate/20250622041650_create_notice_settings.rb
@@ -1,7 +1,7 @@
 class CreateNoticeSettings < ActiveRecord::Migration[8.0]
   def change
     create_table :notice_settings do |t|
-      t.references :user, null: false
+      t.references :user, null: false, foreign_key: true
       t.boolean :like_enable, null: false, default: true
       t.boolean :repost_enable, null: false, default: true
       t.boolean :quote_enable, null: false, default: true
