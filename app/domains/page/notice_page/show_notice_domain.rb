7f21b24a1f82813261322ee9644c76b637d957e1
doguto <doguto110@gmail.com>
2025-06-22 18:09:29 +0900

    Fix: 自分で自分にしたアクションの通知は受け取らないように修正

--- app/domains/page/notice_page/show_notice_domain.rb
+++ app/domains/page/notice_page/show_notice_domain.rb
@@ -6,15 +6,25 @@ module Page::NoticePage
     QUOTE_TYPE = ["Quote"].freeze
 
     def execute
-      notices = Notice.where(user_id: Current.current_user&.id, is_hide: false).includes(:notifiable).order(created_at: :desc)
+      user = Current.current_user
+
+      notices = Notice.where(user_id: user&.id, is_hide: false).includes(:notifiable).order(created_at: :desc)
       notices.map do |notice|
         if FOLLOW_NOTICE_TYPE.include?(notice.notifiable_type)
+          next if notice.notifiable.followed_id == user.id
+
           NoticeDto.new(notice, user: notice.notifiable.follower)
         elsif POST_NOTICE_TYPE.include?(notice.notifiable_type)
+          next if notice.notifiable.user_id == user.id
+
           NoticeDto.new(notice, user: notice.notifiable.user, post: notice.notifiable.post)
         elsif REPLY_TYPE.include?(notice.notifiable_type)
+          next if notice.notifiable.child_post.user_id == user.id
+
           NoticeDto.new(notice, user: notice.notifiable.child_post.user, post: notice.notifiable.child_post)
         elsif QUOTE_TYPE.include?(notice.notifiable_type)
+          next if notice.notifiable.quoting_post.user_id == user.id
+
           NoticeDto.new(notice, user: notice.notifiable.quoting_post.user, post: notice.notifiable.quoting_post)
         else
           NoticeDto.new(notice)
