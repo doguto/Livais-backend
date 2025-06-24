ea1bd2d9fc16f27c4211bc26d01eac5a937172ea
tytdroid <p.0e0.q20@gmail.com>
2025-06-24 14:46:51 +0900

    add username columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_24_020955) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_24_053846) do
   create_table "ai_models", force: :cascade do |t|
     t.string "model"
     t.string "string"
@@ -96,6 +96,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_24_020955) do
     t.string "password_digest"
     t.string "provider"
     t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
