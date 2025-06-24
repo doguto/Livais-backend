d26a6de6d8c507e4bb69cb7542c2fee6c7ad37ef
tytdroid <p.0e0.q20@gmail.com>
2025-06-24 18:36:06 +0900

    add profile_dto

---
+++ app/datatransfers/profile_dto.rb
@@ -0,0 +1,21 @@
+class ProfileDto
+  def initialize(user)
+    @user = user
+  end
+
+  def to_h
+    {
+      user_id: @user.id,
+      user: {
+        name: @user.name,
+        username: @user.username,
+        image: @user.image,
+        cover_image: @user.profile&.cover_image,
+        bio: @user.profile&.bio,
+        join_date: @user.created_at.strftime("%Y-%m-%d"),
+        follow_count: @user.following.count,
+        follower_count: @user.followers.count
+      }.camelize
+    }.camelize
+  end
+end
