5ef6a7b9b775460cf6e06bd9b9f6b0635d094cad
tytdroid <p.0e0.q20@gmail.com>
2025-06-24 14:15:47 +0900

    change profile_domain

--- app/domains/page/profile_page/show_user_with_profile_domain.rb
+++ app/domains/page/profile_page/show_user_with_profile_domain.rb
@@ -6,7 +6,21 @@ module Page::ProfilePage
     end
 
     def execute
-      User.includes(:profile).find(@user_id)
+      user = User.includes(:profile, :followers, :followings).find(@user_id)
+
+      {
+        userId: user.id,
+        user: {
+          name: user.name,
+          username: user.username,
+          image: user.image,
+          cover_image: user.profile&.cover_image,
+          bio: user.profile&.bio,
+          joinDate: user.created_at.strftime("%Y-%m-%d"),
+          followCount: user.followings.count,
+          followerCount: user.followers.count
+        }
+      }
     end
   end
 end
