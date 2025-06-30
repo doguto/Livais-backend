69aaf99ee2aac9c792ab3e561b2786b58d64d7b1
tytdroid <p.0e0.q20@gmail.com>
2025-06-30 13:00:37 +0900

    profile.dto記法統一

--- app/datatransfers/profile_dto.rb
+++ app/datatransfers/profile_dto.rb
@@ -5,16 +5,14 @@ class ProfileDto
 
   def get
     {
-      user_id: @user.id,
-      user: {
-        name: @user.name,
-        image: @user.image,
-        cover_image: @user.profile&.cover_image,
-        bio: @user.profile&.bio,
-        join_date: @user.created_at.strftime("%Y-%m-%d"),
-        follow_count: @user.following.size,
-        follower_count: @user.followers.size
-      }.camelize
+      "id" => @user.id,
+      "name" => @user.name,
+      "image" => @user.image,
+      "cover_image" => @user.profile&.cover_image,
+      "bio" => @user.profile&.bio,
+      "join_date" => @user.created_at.strftime("%Y-%m-%d"),
+      "follow_count" => @user.following.size,
+      "follower_count" => @user.followers.size
     }.camelize
   end
 end
