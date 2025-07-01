13e728c881033303d39d07d3cf96133c7a5157d6
tytdroid <p.0e0.q20@gmail.com>
2025-07-01 14:39:56 +0900

    不必要なマイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_24_094943) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_24_020955) do
   create_table "ai_models", force: :cascade do |t|
     t.string "model"
     t.string "string"
