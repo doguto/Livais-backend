7e28b5c8e35ed3e92851e0125ddddef86c69de9d
doguto <doguto110@gmail.com>
2025-07-03 01:18:55 +0900

    Chore: 他にも不要なファイルがあったので除外

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,13 @@ module LivaisBackend
 
     config.generators do |g|
       g.assets false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine false
       g.skip_routes false
       g.test_framework false
+      g.fixture_replacement false
     end
   end
 end
