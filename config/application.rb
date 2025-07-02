3f8fc9f1b3d647eb4932269c5a1a091e36906ff9
doguto <doguto110@gmail.com>
2025-07-03 01:12:51 +0900

    Chore: 余計なファイルをgeneratorで生成しないように変更

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,11 @@ module LivaisBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_framework false
+    end
   end
 end
