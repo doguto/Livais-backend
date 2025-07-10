a486f2e9caf4d25c8e4f08fce2ae52a7015396a0
doguto <doguto110@gmail.com>
2025-07-10 18:39:41 +0900

    Fix: DiscordのchannelIDを環境変数に

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = "https://b834e55bbeb2a75dc8146cf593813d73@o4509643388682240.ingest.us.sentry.io/4509643398840320"
+  config.dsn = ENV.fetch("SENTRY_DSN")
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Add data like request headers and IP for users,
