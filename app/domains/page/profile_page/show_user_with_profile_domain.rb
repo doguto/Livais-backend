dd5bb197a1ba88661946f60cddbed404b8480a64
tytdroid <p.0e0.q20@gmail.com>
2025-07-25 18:13:59 +0900

    Remove initialize function

--- app/domains/page/profile_page/show_user_with_profile_domain.rb
+++ app/domains/page/profile_page/show_user_with_profile_domain.rb
@@ -1,9 +1,5 @@
 module Page::ProfilePage
   class ShowUserWithProfileDomain < ApplicationDomain
-    def initialize
-      super
-    end
-
     def execute(user_id: Current.current_user&.id)
       user = User.includes(:profile, :followers, :following).find(user_id)
       ProfileDto.new(user)
