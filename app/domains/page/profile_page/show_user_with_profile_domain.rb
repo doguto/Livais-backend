9efd3a00cc0898a9a7961a2125654c5b8a6c1927
tytdroid <p.0e0.q20@gmail.com>
2025-07-25 19:33:34 +0900

    取り込み変更

--- app/domains/page/profile_page/show_user_with_profile_domain.rb
+++ app/domains/page/profile_page/show_user_with_profile_domain.rb
@@ -1,7 +1,7 @@
 module Page::ProfilePage
   class ShowUserWithProfileDomain < ApplicationDomain
     def execute(user_id: Current.current_user&.id)
-      user = User.includes(:profile, :followers, :following).find(user_id)
+      user = User.includes(:profile, :followers, :following).find!(user_id)
       ProfileDto.new(user)
     end
   end
