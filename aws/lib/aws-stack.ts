c905e65d16311f9c7e5333e5aab16adf9a9835ef
doguto <doguto110@gmail.com>
2025-08-07 20:42:08 +0900

    Feat: ec2の設定をcdkで作成

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -1,16 +1,49 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { KeyPair } from "cdk-ec2-key-pair";
+
 
 export class AwsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // VPC
+        // publicSubnet, privateSubnetを各AZに1つずつ作成
+        const vpc = new ec2.Vpc(this, 'DevVpc', {
+            ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+            vpcName: 'DevVpc',
+        })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // Security Group
+        const serverSecurityGroup = new ec2.SecurityGroup(this, 'DevServerSecurityGroup', {
+            vpc
+        })
+        serverSecurityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(22))   // SSHの許可
+        serverSecurityGroup.connections.allowFromAnyIpv4(ec2.Port.tcp(443))  // Httpsの許可
+
+        // EC2 Instance
+        const devServer = new ec2.Instance(this, 'DevServer', {
+            vpc,
+            vpcSubnets: vpc.selectSubnets({
+                subnetType: ec2.SubnetType.PUBLIC,
+            }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO), // 無料枠: t2.micro
+            machineImage: new ec2.AmazonLinuxImage({
+                generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            }),
+            securityGroup: serverSecurityGroup,
+            keyPair: new KeyPair(this, 'DevServerKeyPair', {
+                keyPairName: 'DevServerSshKey',
+                storePublicKey: true
+            }),
+            instanceName: 'DevServer'
+        })
+
+        // CloudFormationへの出力
+        new CfnOutput(this, 'VPC', { value: vpc.vpcId })
+        new CfnOutput(this, 'Security Group', { value: serverSecurityGroup.securityGroupId })
+        new CfnOutput(this, 'EC2', { value: devServer.instanceId })
+    }
 }
