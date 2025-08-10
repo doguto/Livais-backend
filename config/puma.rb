89bf5c860e4bf488502d3a8a8c3f4a4f5b80be3f
EC2 Default User <ec2-user@ip-10-0-0-217.ap-northeast-1.compute.internal>
2025-08-10 06:10:22 +0000

    Chore: temporary-saving

--- config/puma.rb
+++ config/puma.rb
@@ -40,30 +40,15 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
-if Rails.env.development?
-  key_file = Rails.root.join("config", "certs", "localhost.key")
-  cert_file = Rails.root.join("config", "certs", "localhost.cert")
+app_directory = '/var/www/Livais-backend'
+directory app_directory
+rackup "#{app_directory}/config.ru"
+environment 'production'
 
-  unless key_file.exist?
-    root_key = OpenSSL::PKey::RSA.new(4096)
-    key_file.write(root_key)
+bind "unix:///var/www/Livais-backend/tmp/sockets/puma.sock"
 
-    root_cert = OpenSSL::X509::Certificate.new.tap do |root_ca|
-      root_ca.version = 2 # cf. RFC 5280 - to make it a "v3" certificate
-      root_ca.serial = 0x0
-      root_ca.subject = OpenSSL::X509::Name.parse "/C=BE/O=A1/OU=A/CN=localhost"
-      root_ca.issuer = root_ca.subject # root CA"s are "self-signed"
-      root_ca.public_key = root_key.public_key
-      root_ca.not_before = Time.now
-      root_ca.not_after = root_ca.not_before + 2.years # 2 years validity
-      root_ca.sign(root_key, OpenSSL::Digest::SHA256.new)
-    end
-    cert_file.write(root_cert)
-  end
+pidfile "#{app_directory}/tmp/pids/puma.pid"
+state_path "#{app_directory}/tmp/pids/puma.state"
+stdout_redirect "#{app_directory}/log/puma.stdout.log", "#{app_directory}/log/puma.stderr.log", true
 
-  ssl_bind "127.0.0.1", "3000", {
-    key: key_file.to_path,
-    cert: cert_file.to_path
-  }
-end
 
